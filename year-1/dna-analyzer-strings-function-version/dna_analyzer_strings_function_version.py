ac0198f1fc68e51f632d97bc7a485b81855a2679
Zulqarnain <zulqarnainbioinfo@gmail.com>
2026-04-12 12:50:43 +0500

    Add dna_analyzer_strings_function_version

---
+++ year-1/dna-analyzer-strings-function-version/dna_analyzer_strings_function_version.py
@@ -0,0 +1,22 @@
+# DNA Analyzer
+# using string functions
+
+dna_seq ="ATGCGATACGTTAGC"
+# Counting Nucleotides
+count_A = dna_seq.count("A")
+count_T = dna_seq.count("T")
+count_G = dna_seq.count("G")
+count_C = dna_seq.count("C")
+print(f"DNA Sequence :{dna_seq}")
+print("Nucleotide Count :")
+print(f"A :{count_A}")
+print(f"T :{count_T}")
+print(f"G :{count_G}")
+print(f"C :{count_C}")
+
+# Convert to RNA
+rna_seq = dna_seq.replace("T","U")
+print(f"RNA Sequence :{rna_seq}")
+
+# Position of a Codon "ATG"
+print(f"Codon 'ATG' Found at Position :{dna_seq.find("ATG")}")
